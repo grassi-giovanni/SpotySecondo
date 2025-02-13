7e534c3291dda06ee87c1634c295228b18bf8e49
grassi-giovanni <grassi.giovanni@iisgalvanimi.edu.it>
2025-02-13 12:53:41 +0000

    InvalidURI

--- services/spotify_oauth.py
+++ services/spotify_oauth.py
@@ -2,9 +2,9 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 #le tue credenziali le trovi nella dashboard di prima
-SPOTIFY_CLIENT_ID = "9214bb00011c4ac88e9abb2b6ce04ae1"
-SPOTIFY_CLIENT_SECRET = "9412f99b5b05479ba88d0e6fa06d3afd"
-SPOTIFY_REDIRECT_URI = "https://5000-iginiomassa-progettinos-06ic5e6lh2t.ws-eu117.gitpod.io/callback" #dopo il login andiamo qui
+SPOTIFY_CLIENT_ID = "beef1f3d454240eab804424187d37bb6"
+SPOTIFY_CLIENT_SECRET = "12ac03b8466240e5b15f053c42ce55c9"
+SPOTIFY_REDIRECT_URI = "https://5000-grassigiova-spotysecond-i3w6w2glon4.ws-eu117.gitpod.io/callback"
 
 #config SpotifyOAuth per l'autenticazione e redirect uri
 sp_oauth = SpotifyOAuth(
